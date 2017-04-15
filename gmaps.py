75702c1fbcfa5a30c8706d0e6b81a3fdaa72923c
SharonSubathran <sharoncherry@gmail.com>
2017-04-15 05:31:59 -0400

    Update gmaps.py

--- gmaps.py
+++ gmaps.py
@@ -11,23 +11,4 @@ def distance_matrix(client, origins, destinations,
         "destinations": convert.location_list(destinations)
     }
 
-    if departure_time:
-        params["departure_time"] = convert.time(departure_time)
-
-    if arrival_time:
-        params["arrival_time"] = convert.time(arrival_time)
-
-    if departure_time and arrival_time:
-        raise ValueError("Should not specify both departure_time and"
-                         "arrival_time.")
-
-    if transit_mode:
-        params["transit_mode"] = convert.join_list("|", transit_mode)
-
-    if transit_routing_preference:
-        params["transit_routing_preference"] = transit_routing_preference
-
-    if traffic_model:
-        params["traffic_model"] = traffic_model
-
     return client._request("/maps/api/distancematrix/json", params)
